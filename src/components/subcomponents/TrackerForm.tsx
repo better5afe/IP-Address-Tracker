99cbda5f03667f2293bb5a3f2dbc57e22bcb6922
better5afe <abramczyk2704@gmail.com>
2023-09-10 13:24:53 +0200

    header form implemented

--- src/components/subcomponents/TrackerForm.tsx
+++ src/components/subcomponents/TrackerForm.tsx
@@ -1,5 +1,28 @@
-const TrackerForm = () => {
-    return <h1>Tracker Form</h1>
-}
+import './TrackerForm.scss';
 
-export default TrackerForm
+const TrackerForm = () => {
+	const submitFormHandler = (event: React.FormEvent) => {
+		event.preventDefault();
+		console.log('form submitted!');
+    };
+    
+    // form__input--error class to be added to input on error
+
+	return (
+		<form className='form' onSubmit={submitFormHandler}>
+			<input
+				className='form__input'
+				placeholder='Search for an IP'
+				type='text'
+			/>
+			<button className='form__button' type='submit'>
+				<svg xmlns='http://www.w3.org/2000/svg' width='11' height='14'>
+					<path fill='none' stroke='#FFF' stroke-width='3' d='M2 1l6 6-6 6' />
+				</svg>
+			</button>
+			<p className='form__error'>Invalid IP address</p>
+		</form>
+	);
+};
+
+export default TrackerForm;
