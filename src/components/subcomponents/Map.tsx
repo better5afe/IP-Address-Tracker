95fbaaa213391ce077a2e9b2910e5b2e3ef9e0ec
better5afe <abramczyk2704@gmail.com>
2023-09-10 14:52:47 +0200

    custom marker added to the map

--- src/components/subcomponents/Map.tsx
+++ src/components/subcomponents/Map.tsx
@@ -1,18 +1,32 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import { useContext } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import { TrackerContext } from '../../context/tracker-context';
 
-import './Map.scss'
+import './Map.scss';
+
+const newMarker = L.icon({
+	iconUrl: require('../../assets/images/icon-location.png'),
+	iconSize: [46, 56],
+});
+
 
 const Map = () => {
+	const trackerCtx = useContext(TrackerContext);
+
 	return (
-		<MapContainer className='map' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+		<MapContainer
+			className='map'
+			center={[51.505, -0.09]}
+			zoom={18}
+			scrollWheelZoom={false}
+		>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 			/>
-			<Marker position={[51.505, -0.09]}>
-				<Popup>
-					Sample popup
-				</Popup>
+			<Marker position={[51.505, -0.09]} icon={newMarker}>
+				<Popup>{trackerCtx.addressData.location}</Popup>
 			</Marker>
 		</MapContainer>
 	);
