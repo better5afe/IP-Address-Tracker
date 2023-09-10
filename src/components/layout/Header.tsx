b0eae9b81c184a23bb4410ae76a5f0f313a0e585
better5afe <abramczyk2704@gmail.com>
2023-09-10 13:07:11 +0200

    main header part implemented

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,16 @@
 import TrackerForm from '../subcomponents/TrackerForm';
 import TrackerResult from '../subcomponents/TrackerResult';
 
+import './Header.scss';
+
 const Header = () => {
 	return (
-		<header>
-			<TrackerForm />
-			<TrackerResult />
+		<header className='header'>
+			<div className='header__wrapper'>
+				<h1 className='header__title'>IP Address Tracker</h1>
+				<TrackerForm />
+				{/* <TrackerResult /> */}
+			</div>
 		</header>
 	);
 };
